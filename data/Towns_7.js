c2e31bcfbd36025571b42a3325f4651dbbddd15c
teamdhaksha <63536148+teamdhaksha@users.noreply.github.com>
2021-01-24 12:18:10 +0530

    Add files via upload

---
+++ data/Towns_7.js
@@ -0,0 +1,13 @@
+var json_Towns_7 = {
+"type": "FeatureCollection",
+"name": "Towns_7",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "NAME": "Dindigul" }, "geometry": { "type": "Point", "coordinates": [ 77.982771348861391, 10.358148632958947, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Natham" }, "geometry": { "type": "Point", "coordinates": [ 78.229221377354563, 10.225305543991475, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Kodaikanal" }, "geometry": { "type": "Point", "coordinates": [ 77.499987909396893, 10.238801940113582, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Palani" }, "geometry": { "type": "Point", "coordinates": [ 77.521158414689594, 10.452017899569007, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Oddanchatram" }, "geometry": { "type": "Point", "coordinates": [ 77.754021338755379, 10.489328590050144, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Nilakottai" }, "geometry": { "type": "Point", "coordinates": [ 77.850397682699196, 10.166593443980233, -999999.0 ] } }
+]
+}
